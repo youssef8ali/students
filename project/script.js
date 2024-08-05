eedb87a851b1f61f75b40128fa4dfa5a3f5d2856
AlMoataz-M <moatzmohammed222@yahoo.com>
2024-08-06 02:12:38 +0300

    first modify

---
+++ project/script.js
@@ -0,0 +1,8 @@
+let students = [
+  {
+    name: "Al-Moataz Billah Alaa El-Dien Mohammed Ramadan",
+    age: 20,
+    email: "moatzmohammed222@yahoo.com",
+    track: "MERN stack developer",
+  },
+];
